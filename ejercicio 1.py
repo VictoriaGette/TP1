20f1fec840691899930c013c6427273bc308adcd
VictoriaGette <152293571+VictoriaGette@users.noreply.github.com>
2024-05-08 09:14:44 -0300

    Add files via upload

---
+++ ejercicio 1.py
@@ -0,0 +1,12 @@
+# Entero
+entero = 5555
+# Flotante
+flotante = 3.1498371794182973
+# Cadena
+cadena = "No se que hago con mi vida"
+# Booleano
+TrueOrFalse = True
+print("Booleano:", TrueOrFalse)
+print("Cadena:", cadena)
+print("Flotante:", flotante)
+print("Entero:", entero)
